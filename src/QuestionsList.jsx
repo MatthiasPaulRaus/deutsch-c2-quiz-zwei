2d659bba46f30f24fe839f57e618cd63427d0fe1
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-06-30 12:03:32 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Die Personalabteilung ist auf der Suche",
+        answersList: [
+            {answer:"nach einem neuen Informatiker.", isCorrect:true},
+            {answer:"nach eine neue Mitarbeiterin.", isCorrect:false},
+            {answer:"nach neuen Auszubildende.", isCorrect:false},
+        ]
+    },{
+        question:"Beim Bewerbungsgespräch sollen die Kandidaten",
+        answersList: [
+            {answer:"vorstellen, wen oder was sie kennen.", isCorrect:false},
+            {answer:"ihre Kenntnisse unter Beweis stellen.", isCorrect:true},
+            {answer:"ihre Vorstellung beweisen.", isCorrect:false},
+        ]
+    },{
+        question:"Nur hochqualifizierte Bewerbende",
+        answersList: [
+            {answer:"sollen darauf Rücksicht nehmen.", isCorrect:false},
+            {answer:"werden bei uns nicht berücksichtigt.", isCorrect:false},
+            {answer:"können hier Berücksichtigung finden.", isCorrect:true},
+        ]
+    },{
+        question:"Die Fragen der Personalleitung",
+        answersList: [
+            {answer:"nahmen Bezug auf die Sprachkenntnisse der Bewerbenden.", isCorrect:true},
+            {answer:"beziehen sich auf den Personalausweis.", isCorrect:false},
+            {answer:"setzen sich mit den Bewerbenden auseinander.", isCorrect:false},
+        ]
+    },{
+        question:"Das Erscheinungsbild der Kandidaten",
+        answersList: [
+            {answer:"hat eine Nachwirkung auf die Bewerbenden.", isCorrect:false},
+            {answer:"hat Auswirkung auf das Auswahlverfahren.", isCorrect:true},
+            {answer:"sollte sich auf das Gespräch auswirken.", isCorrect:false},
+        ]
+    },{
+        question:"Am Ende des Bewerbungsverfahrens",
+        answersList: [
+            {answer:"müssen die Personaler die Kandidaten finden.", isCorrect:false},
+            {answer:"entscheiden die Bewerbenden eine neue Aufgabe.", isCorrect:false},
+            {answer:"treffen die Personaler eine Entscheidung.", isCorrect:true},
+        ]
+    },{
+        question:"Die freien Mitarbeitenden leisten ",
+        answersList: [
+            {answer:"einen wichtigen Beitrag zur Entwicklung des Projekts.", isCorrect:true},
+            {answer:"sich einen Erfolg bei der Entwicklung des Unternehmens.", isCorrect:false},
+            {answer:"keine Teilnahme am Projekt des Unternehmens.", isCorrect:false},
+        ]
+    },{
+        question:"Der Personaler hegte Zweifel an der Eignung des Kandidaten.",
+        answersList: [
+            {answer:"Er vermutete, dass der Bewerbende Zweifel hatte.", isCorrect:false},
+            {answer:"Er glaubte, dass der Kandidat nicht geeignet war.", isCorrect:true},
+            {answer:"Er verzweifelte an den zahlreichen Bewerbenden.", isCorrect:false},
+        ]
+    },{
+        question:"Nach dem Bewerbungsgespräch erhielten die abgelehnten Bewerbenden",
+        answersList: [
+            {answer:"eine persönliche Benachrichtigung des Personalchefs.", isCorrect:false},
+            {answer:"eine schriftliche Zusage der Unternehmensleitung.", isCorrect:false},
+            {answer:"schriftlich Nachricht von der Personalabteilung.", isCorrect:true},
+        ]
+    },{
+        question:"Die ausgewählten Bewerbenden werden persönlich",
+        answersList: [
+            {answer:"von der Personalabteilung kontaktiert.", isCorrect:true},
+            {answer:"mit den Personalern in Verbindung gebracht.", isCorrect:false},
+            {answer:"durch Kontaktieren der Personalabteilung informiert.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Die Partei hat bei den Kommunalwahlen",
+        answersList: [
+            {answer:"eine empfindliche Niederlage erlitten.", isCorrect:true},
+            {answer:"einen großen Erfolg erlitten.", isCorrect:false},
+            {answer:"keinen bemerkenswerten Zuwachs erlitten.", isCorrect:false},
+        ]
+    },{
+        question:"In der Firma werden Personalwechsel in Erwägung gezogen.",
+        answersList: [
+            {answer:"Die Veränderungen werden sofort umgesetzt.", isCorrect:false},
+            {answer:"Man hat die Absicht personelle Veränderungen vorzunehmen.", isCorrect:true},
+            {answer:"Man rechnet nicht mit baldigen Neubesetzungen.", isCorrect:false},
+        ]
+    },{
+        question:"Durch die Weiterbildung versuchen wir",
+        answersList: [
+            {answer:"uns beruflich mit Bildung zu beschäftigen.", isCorrect:false},
+            {answer:"ein berufliches Ziel zu definieren.", isCorrect:false},
+            {answer:"eine Verbesserung der Fachkenntnisse zu erzielen.", isCorrect:true},
+        ]
+    },{
+        question:"Durch finanzielle Anreize versucht die Firma",
+        answersList: [
+            {answer:"die Leistungsfähigkeit der Mitarbeiter zu erhöhen.", isCorrect:true},
+            {answer:"die Arbeitnehmer geringer zu entlohnen.", isCorrect:false},
+            {answer:"den Kollegen weitere Leistungen zu kürzen.", isCorrect:false},
+        ]
+    },{
+        question:"Gestern haben meine Kollegen mir einen Bären aufgebunden.",
+        answersList: [
+            {answer:"Ich habe immer noch große Rückenschmerzen.", isCorrect:false},
+            {answer:"Sie haben mir eine glatte Lüge erzählt.", isCorrect:true},
+            {answer:"Sie haben mir eine wichtige Nachricht mitgeteilt.", isCorrect:false},
+        ]
+    },{
+        question:"Wer immer die Wahrheit sagt, kommt damit am Weitesten.",
+        answersList: [
+            {answer:"Ach so! Wer lügt hat kurze Beine.", isCorrect:false},
+            {answer:"Klar! Also Vorsicht beim Märchenvorlesen.", isCorrect:false},
+            {answer:"Meinst du damit, dass ehrlich am Längsten währt?", isCorrect:true},
+        ]
+    },{
+        question:"Die bereits getroffenen Maßnahmen können",
+        answersList: [
+            {answer:"nicht mehr rückgängig gemacht werden.", isCorrect:true},
+            {answer:"leider auch in Zukunft nicht umgesetzt werden.", isCorrect:false},
+            {answer:"rückwirkend entschieden werden.", isCorrect:false},
+        ]
+    },{
+        question:"Durch das Praktikum können Sie",
+        answersList: [
+            {answer:"Ihre Kenntnisse auch anderen vermitteln.", isCorrect:false},
+            {answer:"Ihre fachlichen Fertigkeiten weiter entwickeln.", isCorrect:true},
+            {answer:"Ihre Ausbildung wahrscheinlich früher beenden.", isCorrect:false},
+        ]
+    },{
+        question:"Welche Gründe waren für Ihre Bewerbung ausschlaggebend?",
+        answersList: [
+            {answer:"Ich wollte mehr Freizeit mit Freunden verbringen.", isCorrect:false},
+            {answer:"Ich wollte weniger Ärger mit den Kollegen haben.", isCorrect:false},
+            {answer:"Ich wollte mich vor allem fachlich weiterbilden.", isCorrect:true},
+        ]
+    },{
+        question:"Bei dem Seminar hat der Projektleiter Partei für seine Mitarbeiter ergriffen.",
+        answersList: [
+            {answer:"Er verteidigte die Kollegen gegenüber vorgebrachter Kritik.", isCorrect:true},
+            {answer:"Er präsentierte die neuen Forderungen der Gewerkschaft.", isCorrect:false},
+            {answer:"Er stellte eine weitere alternative Sichtweise vor.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Den Antrag auf Erstattung der Reisekosten habe ich schon gestellt.",
+        answersList: [
+            {answer:"Die Sachbearbeiterin hat dem Antrag stattgegeben.", isCorrect:true},
+            {answer:"Die Finanzabteilung hat nicht nachgegeben.", isCorrect:false},
+            {answer:"Du hättest es vor der Dienstreise beantragen sollen.", isCorrect:false},
+        ]
+    },{
+        question:"Die Personalabteilung hat ihm weitere Aufstiegsmöglichkeiten",
+        answersList: [
+            {answer:"zur Ansicht bereitgestellt.", isCorrect:false},
+            {answer:"in Aussicht gestellt.", isCorrect:true},
+            {answer:"angesichts seiner hohen Leistung erschwert.", isCorrect:false},
+        ]
+    },{
+        question:"Das ständige Nörgeln und Meckern",
+        answersList: [
+            {answer:"hat meine Planung hervorragend unterstützt.", isCorrect:false},
+            {answer:"war ein wichtiger Beitrag zur Lösung des Konflikts.", isCorrect:false},
+            {answer:"hat mir das Wochenende gründlich vermiest.", isCorrect:true},
+        ]
+    },{
+        question:"Durch Ihre Unterstützung gewährleisten Sie",
+        answersList: [
+            {answer:"den Fortbestand unserer Untersuchungsreihe.", isCorrect:true},
+            {answer:"dass unsere Projektarbeit eingestellt wird.", isCorrect:false},
+            {answer:"dass unsere Arbeit weiterhin behindert wird.", isCorrect:false},
+        ]
+    },{
+        question:"Mein Kollege hat mir etwas Persönliches anvertraut.",
+        answersList: [
+            {answer:"Freut mich, dass ihr euch trotzdem so gut versteht.", isCorrect:false},
+            {answer:"Klischees sollte man grundsätzlich in Frage stellen.", isCorrect:false},
+            {answer:"Gegenseitiges Vertrauen ist sehr wichtig dabei.", isCorrect:true},
+        ]
+    },{
+        question:"Seine anhaltend schlechte Laune",
+        answersList: [
+            {answer:"führte zur Demotivation der Mitarbeiter.", isCorrect:true},
+            {answer:"war ein Beweis für seine fachliche Eignung.", isCorrect:false},
+            {answer:"brachte die Mitarbeiter dazu ihn zu unterstützen.", isCorrect:false},
+        ]
+    },{
+        question:"Für die bevorstehenden Wochen sollten wir",
+        answersList: [
+            {answer:"alle notwendigen Mittel veruntreuen.", isCorrect:false},
+            {answer:"alle notwendigen Vorbereitungen treffen.", isCorrect:true},
+            {answer:"mit allen Vorkehrungen in Verzug geraten.", isCorrect:false},
+        ]
+    },{
+        question:"Wer wurde für das Versagen zur Verantwortung gezogen?",
+        answersList: [
+            {answer:"Ein paar Vertraute, die es weitergesagt hatten.", isCorrect:false},
+            {answer:"Die Mitarbeiter, die den Schaden erlitten haben.", isCorrect:false},
+            {answer:"Alle, die an dem genannten Projekt beteiligt waren.", isCorrect:true},
+        ]
+    },{
+        question:"Unter Umständen können wir fachlich zur Seite stehen.",
+        answersList: [
+            {answer:"Vielen Dank für Ihre professionelle Hilfe.", isCorrect:true},
+            {answer:"Ihr großzügiges Angebot hat uns alle enttäuscht.", isCorrect:false},
+            {answer:"Ihre Unterstützung war nicht mehr notwendig.", isCorrect:false},
+        ]
+    },{
+        question:"Wir haben Ihren Bedenken hohe Aufmerksamkeit geschenkt.",
+        answersList: [
+            {answer:"Deshalb sollten wir Sie dafür entschädigen.", isCorrect:false},
+            {answer:"Ihre Forderungen sind durchaus gerechtfertigt.", isCorrect:false},
+            {answer:"Wir sind sehr an Ihren Vorschlägen interessiert.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
